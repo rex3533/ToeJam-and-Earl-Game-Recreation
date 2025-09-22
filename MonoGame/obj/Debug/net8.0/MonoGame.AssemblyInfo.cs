75b491f590b05cbd5d698dc13f57ef6e9d771be1
GCezarotto <glayconcezarotto@lewis.edu>
2025-09-22 09:53:38 -0500

    pull and commit

--- MonoGame/obj/Debug/net8.0/MonoGame.AssemblyInfo.cs
+++ MonoGame/obj/Debug/net8.0/MonoGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+956e4ae07a6685cd9b82b7f215f3c5612e2d1b89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61eefb804040371a72866f9a526c4eca147b08ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
